7bd5778647bfabd8098260516efae3b2f01111d9
lvanish <30948972+lvanish@users.noreply.github.com>
2018-10-26 12:58:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rasesh/whatsappclone/MessageAdapter.java
@@ -0,0 +1,128 @@
+package com.example.rasesh.whatsappclone;
+
+import android.graphics.Color;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by Rasesh on 10/24/2018.
+ */
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+
+    private List<Messages> userMessagesList;
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
+
+
+    public MessageAdapter (List<Messages> userMessagesList){
+        this.userMessagesList = userMessagesList;
+    }
+
+
+    public class MessageViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView senderMessageText, receiverMessageText;
+        public CircleImageView receiverProfileImage;
+
+        public MessageViewHolder(View itemView) {
+            super(itemView);
+
+            senderMessageText = (TextView)itemView.findViewById(R.id.sender_message_text);
+            receiverMessageText = (TextView)itemView.findViewById(R.id.receiver_message_text);
+            receiverProfileImage = (CircleImageView) itemView.findViewById(R.id.message_profile_image);
+        }
+    }
+
+
+
+    @Override
+    public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.custom_messages_layout, parent, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        return new MessageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final MessageViewHolder holder, int position) {
+
+        String messageSenderId = mAuth.getCurrentUser().getUid();
+        Messages messages = userMessagesList.get(position);
+
+        String fromUserID = messages.getFrom();
+        String fromMessageType = messages.getType();
+
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(fromUserID);
+
+        UsersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.hasChild("image")){
+                    String  receiverImage = dataSnapshot.child("image").getValue().toString();
+
+                    Picasso.get().load(receiverImage).placeholder(R.drawable.profile_pic_icon).into(holder.receiverProfileImage);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        if(fromMessageType.equals("text")){
+
+            holder.receiverMessageText.setVisibility(View.INVISIBLE);
+            holder.receiverProfileImage.setVisibility(View.INVISIBLE);
+            holder.senderMessageText.setVisibility(View.INVISIBLE);
+
+            if(fromUserID.equals(messageSenderId)){
+
+                holder.senderMessageText.setVisibility(View.VISIBLE);
+                holder.senderMessageText.setBackgroundResource(R.drawable.sender_messages_layout);
+                holder.senderMessageText.setTextColor(Color.BLACK);
+                holder.senderMessageText.setText(messages.getMessage());
+
+            }
+            else{
+
+                holder.receiverProfileImage.setVisibility(View.VISIBLE);
+                holder.receiverMessageText.setVisibility(View.VISIBLE);
+
+                holder.receiverMessageText.setBackgroundResource(R.drawable.receiver_messages_layout);
+                holder.receiverMessageText.setTextColor(Color.BLACK);
+                holder.receiverMessageText.setText(messages.getMessage());
+            }
+
+        }
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return userMessagesList.size();
+    }
+
+}
---
+++ app/src/main/java/com/example/rasesh/whatsappclone/TabsAccessorAdapter.java
@@ -0,0 +1,60 @@
+package com.example.rasesh.whatsappclone;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Rasesh on 10/4/2018.
+ */
+
+public class TabsAccessorAdapter extends FragmentPagerAdapter {
+
+    public TabsAccessorAdapter(FragmentManager fm){
+        super(fm);
+    }
+    @Override
+    public Fragment getItem(int position) {
+
+        switch(position)
+        {
+            case 0:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+            case 1:
+                GroupsFragment groupsFragment = new GroupsFragment();
+                return groupsFragment;
+            case 2:
+                ContactsFragment contactsFragment = new ContactsFragment();
+                return contactsFragment;
+            case 3:
+                RequestsFragment requestsFragment = new RequestsFragment();
+                return requestsFragment;
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position)
+        {
+            case 0:
+                return "Chats";
+            case 1:
+                return "Groups";
+            case 2:
+                return "Contacts";
+            case 3:
+                return "Requests";
+            default:
+                return null;
+        }
+    }
+}
